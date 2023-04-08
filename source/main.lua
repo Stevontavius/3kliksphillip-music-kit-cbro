59abb67aa62b05ade96ecb38d660e61fa7137c18
Steven <80566162+Stevontavius@users.noreply.github.com>
2023-04-08 17:56:04 -0400

    Create main.lua

---
+++ source/main.lua
@@ -0,0 +1,33 @@
+local Music1 = game:GetService("Players").LocalPlayer.PlayerGui.Music.ValveT
+local Music2 = game:GetService("Players").LocalPlayer.PlayerGui.Music.ValveCT
+
+
+--Sounds
+Music1.Win.Value = getsynasset('win.wav')
+Music1.Lose.Value = getsynasset('lose.wav')
+Music1.Bomb.Value = getsynasset('bomb.wav')
+Music1["10"].Value = getsynasset('tenseconds.wav')
+Music1.StartRound["1"].Value = getsynasset('startround.wav')
+Music1.StartAction["1"].Value = getsynasset('startaction.wav')
+
+Music2.Win.Value = getsynasset('win.wav')
+Music2.Lose.Value = getsynasset('lose.wav')
+Music2.Bomb.Value = getsynasset('bomb.wav')
+Music2["10"].Value = getsynasset('tenseconds.wav')
+Music2.StartRound["1"].Value = getsynasset('startround.wav')
+Music2.StartAction["1"].Value = getsynasset('startaction.wav')
+
+--Volume Editors
+Music1.Win.Volume.Value = 5
+Music1.Lose.Volume.Value = 5
+Music1.Bomb.Volume.Value = 5
+Music1["10"].Volume.Value = 5
+Music1.StartRound["1"].Volume.Value = 5
+Music1.StartAction["1"].Volume.Value = 5
+
+Music2.Win.Volume.Value = 5
+Music2.Lose.Volume.Value = 5
+Music2.Bomb.Volume.Value = 5
+Music2["10"].Volume.Value = 5
+Music2.StartRound["1"].Volume.Value = 5
+Music2.StartAction["1"].Volume.Value = 5
